41119d020fffda7f4b4698eaf0ceb0a7bd263f63
shank <shank@LAPTOP-IVAB678Q>
2021-04-19 17:07:57 +0530

    add

--- 450/src/String/CheckPalindrome.java
+++ 450/src/String/CheckPalindrome.java
@@ -2,9 +2,25 @@ package String;
 
 public class CheckPalindrome {
 
+	 public static int isPlaindrome(String s) {
+	        // code here
+	        int n=s.length();
+	        int i=0;
+	        int j=n-1;
+	        while(i<j){
+	            if(s.charAt(i)==s.charAt(j)){
+	                i++;
+	                j--;
+	            }else 
+	                return 0;
+	        }
+	        return 1;    
+	    }
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String s="abba";
+		System.out.println(isPlaindrome(s));
 	}
 
 }
