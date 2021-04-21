3d1868db1b9f282ba205f73c7b53595a2f2932e7
shank <shank@LAPTOP-IVAB678Q>
2021-04-21 12:44:30 +0530

    add

---
+++ 450/src/String/StringRotation.java
@@ -0,0 +1,28 @@
+package String;
+
+public class StringRotation {
+
+	public static boolean areRotations(String s1,String s2) {
+		if(s1.length()!=s2.length()) {
+			return false;
+		}
+					
+		String temp=s1+s1;
+		if(temp.contains(s2)) {
+			return true;
+		}
+		
+
+
+
+		return false;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1="ABCD";
+		String s2="CDAB";
+		System.out.println(areRotations(s1, s2));
+	}
+
+}
